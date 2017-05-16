376ee3392400b5aed10d4cbcf6d4b832225bb9a8
zwh <751006992@qq.com>
2017-05-16 17:52:55 +0800

    音量调节功能

--- src/components/Musicinfo.vue
+++ src/components/Musicinfo.vue
@@ -11,11 +11,23 @@
 		</mt-header>
 	
 		<!--播放唱片-->
-		<div class="phonograph" :style="{height:allheight}" >
-			<div class="phonograph-record" :class="{pause:!playStatus}">
+		<div class="phonograph" :style="{height:allheight}">
+			<!--封面-->
+			<div class="phonograph-record" :class="{pause:!playStatus}" @click="isLyr()" v-show="!isShowLyr">
 				<div class="imgbox">
-					<img :src="song.album.blurPicUrl">
+					<img :src="song.album.blurPicUrl|picSize">
 				</div>
+			</div>
+			<!--歌词-->
+			<div class="lyricsBox" v-show="isShowLyr" @click="isLyr()">
+				<!--音量调节-->
+				<mt-range v-model="currentVolume" :min="0" :max="100" >
+					<div slot="start" class="bef">
+						<i class="fa fa-fw" :class="{'fa-volume-off':currentVolume==0,'fa-volume-down':currentVolume<=50&&currentVolume>0,'fa-volume-up':currentVolume>50}" @click.stop="mute()"></i>
+						{{currentVolume}}
+					</div>
+  					<div slot="end"  class="aft">100</div>
+				</mt-range>
 			</div>
 			<audio :src="song.mp3Url" id="songAudio"></audio>
 		</div>
@@ -40,7 +52,7 @@
 			</div>
 			<div class="progressBar">
 				<mt-progress :value="progressVal">
-					<div slot="start" class="w5">{{playNowTime}}</div>
+					<div slot="start" class="w5">{{currentTime|countCurTime}}</div>
 					<div slot="end" class="w5">{{song.bMusic.playTime|playtime}}</div>
 				</mt-progress>
 			</div>
@@ -76,59 +88,93 @@ export default {
 		return {
 			doArea: 150,
 			song: {
-				album:{
-					blurPicUrl:''
+				album: {
+					blurPicUrl: ''
 				},
-				bMusic:{
-					playTime:''
+				bMusic: {
+					playTime: ''
 				}
 			},
 			playStatus: false,//判断音乐播放状态 false 关闭 true 播放
 			playIcon: playIcon,//播放、暂停按妞icon
-			progressVal:0,//当前播放进度条
-			progress:null,//动态进度条计时器
-			playNowTime:"00:00",//当前播放时间
-			islike:false
+			progressVal: 0,//当前播放进度条
+			progress: null,//动态进度条计时器
+			currentTime: 0,//当前播放时间
+			islike: false,//是否添加到了喜欢的音乐列表中
+			isShowLyr: false,//歌词显示状态
+			currentVolume:20//当前音量
 		}
 	},
 	methods: {
 		getInfo() {
 			servers.get('/song/' + this.$route.params.id, result => {
-				console.log(result.data[0])
+				// console.log(result.data[0])
 				this.song = result.data[0];
+				// 初始化音量
+				const auiodDom = document.getElementById("songAudio");
+				auiodDom.volume =this.currentVolume/100;
 			});
-			
+
 		},
+		// 播放或暂停
 		play() {
-			const auiodDom=document.getElementById("songAudio");
+			const auiodDom = document.getElementById("songAudio");
 			if (this.playStatus) {//播放中
 				this.playIcon = playIcon;
 				auiodDom.pause();
-				this.playStatus=false;
+				this.playStatus = false;
 				clearInterval(this.progress);
 			} else {//暂停中
 				this.playIcon = pauseIcon;//切换播放按妞样式
-				auiodDom.volume=0.2;
-				this.progress=setInterval(()=>{
+				this.progress = setInterval(() => {
 					// 进度条
-					var currentTime=auiodDom.currentTime;
-					currentTime=Math.round(currentTime*1000);
-					this.progressVal= Math.round(currentTime/this.song.bMusic.playTime*100); 
-					//TODO:当前播放时间
-				},1000)
+					var currentTime;
+					this.currentTime = auiodDom.currentTime;
+					currentTime = Math.round(this.currentTime * 1000);
+					this.progressVal = Math.round(currentTime / this.song.bMusic.playTime * 100);
+				}, 1000)
 				auiodDom.play();
-				this.playStatus=true;
+				this.playStatus = true;
 			}
 		},
 		// 喜欢
-		isLike(){
-			if(this.islike){
-				this.islike=false
+		isLike() {
+			if (this.islike) {
+				this.islike = false
 				Toast('已取消喜欢');
-			}else{
-				this.islike=true;
+			} else {
+				this.islike = true;
 				Toast('已添加到我喜欢的音乐');
 			}
+		},
+		//显示歌词
+		isLyr() {
+			if (this.isShowLyr) {
+				this.isShowLyr = false;
+			} else {
+				this.isShowLyr = true;
+			}
+		},
+		// 静音或放开静音
+		mute(){
+			if(this.currentVolume){//静音操作
+				// 如果是静音操作，在静音前先把之前的音量值保存起来
+				sessionStorage.setItem("vol",this.currentVolume);
+				this.currentVolume=0;
+			}else{//放开静音
+				if(sessionStorage.getItem('vol')){//若之前保存的值存在，则还原成之前保存的值
+					this.currentVolume=Number(sessionStorage.getItem('vol'));
+				}else{//如果保存的值丢失,则默认改为20
+						this.currentVolume=20;
+				}
+			}
+			
+		}
+	},
+	watch: {
+		currentVolume(n,o){
+			const auiodDom=document.getElementById("songAudio");
+			auiodDom.volume=n/100;
 		}
 	},
 	computed: {
@@ -140,8 +186,9 @@ export default {
 		}
 	},
 	filters: {
+		// 转换歌曲总时间
 		playtime(value) {
-			value=Math.round(value/1000);
+			value = Math.ceil(value / 1000);
 			var min = Math.floor(value / 60);
 			var sec = value % 60;
 			if (min < 10) {
@@ -149,6 +196,23 @@ export default {
 			}
 			if (sec < 10) {
 				sec = "0" + sec
+			}
+			return min + ':' + sec;
+		},
+		// 转化歌曲封面尺寸
+		picSize(value) {
+			return value + "?param=182y182";
+		},
+		// 转化当前播放时间为字符串
+		countCurTime(value) {
+			value = Math.ceil(Number(value));
+			var min = Math.floor(value / 60);;
+			var sec = value % 60;
+			if (sec < 10) {
+				sec = "0" + sec
+			}
+			if (min < 10) {
+				min = "0" + min;
 			}
 			return min + ':' + sec;
 		}
@@ -164,18 +228,18 @@ export default {
 @import '../sass/common';
 
 @include keyframes(whirl) {
-    0%   { 
+	0% {
 		@include transform(rotate(0deg))
-    }
-    100% { 
-        @include transform(rotate(360deg))
-    }
+	}
+	100% {
+		@include transform(rotate(360deg))
+	}
 }
+
 .musicinfo {
 	background-image: linear-gradient(to top, #000000 0%, #434343 100%);
 	header {
 		background: transparent !important;
-		border-bottom: 1px solid #ccc;
 		.mintui-back {
 			color: #fff;
 		}
@@ -191,7 +255,7 @@ export default {
 		align-items: center;
 		video {
 			display: none;
-		}
+		} // 封面部分
 		.phonograph-record {
 			$record-height: 280px;
 			@include flex;
@@ -204,8 +268,8 @@ export default {
 			background: url(../assets/coverall.png) no-repeat;
 			background-size: 532px 2093px;
 			background-position: -200px -815px;
-			@include animate(whirl 5s linear  infinite);
-			&.pause{
+			@include animate(whirl 5s linear infinite);
+			&.pause {
 				@include animation-play-state(paused);
 			}
 			.imgbox {
@@ -219,6 +283,24 @@ export default {
 					width: 100%;
 					height: 100%;
 					border-radius: 50%;
+				}
+			}
+		} // 歌词部分
+		.lyricsBox {
+			width: 90%;
+			margin: 0 auto;
+			height: 100%;
+			padding: 12px 0;
+			// 音量调节
+			.mt-range{
+				.bef,.aft{
+					color: #fff;
+					display: inline;
+					margin: 0 5px;
+					i{
+						font-size: 24px;
+						vertical-align: middle;
+					}
 				}
 			}
 		}
@@ -239,7 +321,7 @@ export default {
 				i {
 					color: #fff;
 					font-size: 22px;
-					&.heartred{
+					&.heartred {
 						color: $baseColor;
 					}
 				}
