32529de7466d66d0298fb15d49569657306266cc
zwh <751006992@qq.com>
2017-05-30 16:57:22 +0800

    build

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store'; 
-Vue.config.productionTip = false;
-// import './lib/mui.min.css';
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import servers from './lib/servers'
 import 'font-awesome'
+Vue.config.productionTip = false;
 
 Vue.use(MintUI);
 new Vue({
