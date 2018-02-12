58ae7337a31db33c7e501d418776e755fa55933f
zwh <751006992@qq.com>
2018-02-12 19:55:24 +0800

    朋友页面

---
+++ src/com/friendInfoList.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="friendinfo-box">
+    <!-- 发送动态或视频 -->
+    <div class="sendBtn">
+      <div class="btn-box">
+        <i class="fa fa-edit fa-fw"></i>发动态
+      </div>
+      <div class="btn-box">
+        <i class="fa fa-video-camera fa-fw"></i>发布视频
+      </div>
+    </div>
+    <div class="hr"></div>
+    <div class="hotTopic">
+      <h3 class="hotTopic-title">热门话题
+        <i class="fa fa-angle-right fa-fw"></i>
+      </h3>
+      <div class="hotTopic-scrollBox">
+        <ul class="hotTopic-list">
+          <li class="hotTopic-list-item" v-for="b in 3" :key="b">
+            <span class="words">#{{b}}一首歌预测你的情人节#</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      h: 1
+    };
+  },
+  methods: {},
+  created() {
+    console.log(1);
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "../sass/common";
+.friendinfo-box {
+  .hr {
+    width: 100%;
+    height: 8px;
+    border: 1px solid #e9eaec;
+    border-right: 0;
+    border-left: 0;
+    background: #eeeff1;
+  }
+  .sendBtn {
+    height: 40px;
+    width: 100%;
+    @include flex;
+    .btn-box {
+      position: relative;
+      @include flex-1;
+      text-align: center;
+      line-height: 40px;
+      font-size: 16px;
+      &:first-of-type::after {
+        content: "";
+        position: absolute;
+        right: 0;
+        top: 4px;
+        display: block;
+        width: 1px;
+        height: 32px;
+        background: #eaebed;
+      }
+      i {
+        color: $baseColor;
+        font-size: 18px;
+        vertical-align: middle;
+        margin-right: 5px;
+      }
+    }
+  }
+  // 热门话题
+  .hotTopic {
+    width: 100%;
+    height: 150px;
+    border-bottom: 1px solid #eee;
+    .hotTopic-title {
+      height: 30px;
+      line-height: 30px;
+      font-size: 16px;
+      padding: 0 10px;
+      margin: 10px 0;
+      color: #555;
+    }
+    .hotTopic-scrollBox {
+      height: 80px;
+      width: 100%;
+      padding-left: 10px;
+      overflow-x: auto;
+      .hotTopic-list {
+        height: 100%;
+        width: 488px;
+        -webkit-overflow-scrolling: touch;
+        .hotTopic-list-item {
+          @include flex;
+          justify-content: center;
+          align-items: center;
+          position: relative;
+          height: 100%;
+          width: 156px;
+          background: #eee;
+          margin-right: 10px;
+          float: left;
+          background: rgba(0,0,0,.2);
+          &:last-of-type {
+            margin-right: 0;
+          }
+          .words{
+            display: inline-block;
+            width: 60%;
+            color: #fff;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+
+
--- src/components/findmusic/Friends.vue
+++ src/components/findmusic/Friends.vue
@@ -1,23 +1,147 @@
 <template>
   <div class="friends">
-    <h1>{{ msg }}</h1>
+    <!-- 自定义header -->
+    <div class="diy-header">
+      <div class="header-icon left-icon">
+        <img src="../../assets/icon/user_add.svg" alt="添加好友">
+      </div>
+      <div class="center-box">
+        <div class="keys-btn" :class="{left:tagStatus==1,right:tagStatus==2}">
+          <span @click="changeTag(1)">动态</span>
+          <span @click="changeTag(2)">附近</span>
+          <i class="slide-bar"></i>
+        </div>
+      </div>
+      <div class="header-icon right-icon">
+        <img src="../../assets/icon/musicfrequency.svg" class="frequency" alt="播放音乐">
+      </div>
+    </div>
+
+    <!-- 两个组件 -->
+    <keep-alive>
+      <friendinfolist v-if="tagStatus==1"></friendinfolist>
+    </keep-alive>
+    <keep-alive>
+      <nearby v-if="tagStatus==2"></nearby>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import friendinfolist from '@/com/friendInfoList';
+import nearby from '@/com/nearby';
 export default {
-  name: 'friends',
-  data () {
+  name: "friends",
+  data() {
     return {
-      msg: '我是朋友 页面'
+      tagStatus: 1,
+    };
+  },
+  methods: {
+    changeTag(num = 1) {
+      if (num == this.tagStatus) return;
+      this.tagStatus = num;
     }
+  },
+  components: {
+    friendinfolist,
+    nearby
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style scoped lang="scss">
+@import "../../sass/common";
+.diy-header {
+  $header-height: 40px;
+  width: 100%;
+  height: $header-height;
+  line-height: $header-height;
+  background: $baseColor;
+  .header-icon {
+    position: relative;
+    width: $header-height;
+    height: 100%;
+    &.left-icon {
+      float: left;
+    }
+    &.right-icon {
+      float: right;
+    }
+    img {
+      $img-size: 26px;
+      position: absolute;
+      left: ($header-height - $img-size)/2;
+      top: ($header-height - $img-size)/2;
+      width: $img-size;
+      height: $img-size;
+    }
+  }
+  .center-box {
+    position: relative;
+    display: inline-block;
+    width: calc(100% - #{ $header-height} * 2);
+    height: 100%;
+    .keys-btn {
+      $w: 120px;
+      position: absolute;
+      left: calc(50% - #{$w} / 2);
+      top: calc(50% - 15px);
+      width: $w;
+      height: 30px;
+      box-shadow: 0 0 0 1px  #fff;
+      border-radius: 30px;
+      font-size: 0;
+      span {
+        position: absolute;
+        display: inline-block;
+        color: #fff;
+        line-height: 30px;
+        height: 100%;
+        width: 50%;
+        font-size: 16px;
+        text-align: center;
+        z-index: 2;
+        border-radius: 30px;
+        &:first-of-type {
+          left: 0;
+        }
+        &:last-of-type {
+          left: 50%;
+        }
+      }
+      .slide-bar {
+        position: absolute;
+        left: 0;
+        top: 0;
+        height: 100%;
+        width: 50%;
+        background: #fff;
+        border-radius: 30px;
+        z-index: 1;
+        @include transition(all 0.3s);
+      }
+      &.right {
+        span {
+          &:last-of-type {
+            color: $baseColor;
+          }
+        }
+        .slide-bar {
+          left: calc(50% + 1px);
+        }
+      }
+      &.left {
+        span {
+          &:first-of-type {
+            color: $baseColor;
+          }
+        }
+        .slide-bar {
+          left: -1px;
+        }
+      }
+    }
+  }
 }
 </style>
