a3be8508bf9c97c84c74e67792770d6560da8677
e <751006992@qq.com>
2018-04-18 19:04:00 +0800

    朋友-附近

--- src/com/nearby.vue
+++ src/com/nearby.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="nearby">
-    <h1>{{msg}}</h1>
+    <!-- <h1>{{msg}}</h1> -->
+    <div class="ad-item">
+      <div class="ad-img imgbox"><img src="../assets/ad/gxjw.jpg" alt="活动广告"></div>
+      <div class="ad-des">
+        <p>高校街舞挑战赛，不燃不痛快！</p>
+        <p class="ad-ads">网易云音乐 888km</p>
+        </div>    
+    </div>
   </div>
 </template>
 <script>
 export default {
-  data(){
+  data() {
     return {
-      msg:'附近'
-    }
+      msg: "附近"
+    };
   },
-  created () {
-    console.log(2)
+  created() {
+    console.log(2);
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 
